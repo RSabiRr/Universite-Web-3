c2ca0c31539b8cba1369ac2da20b89bd1b562506
tu201806274 <tu201806274@213-4>
2022-03-09 14:10:34 +0400

    Add project files.

---
+++ Universite Web/Models/CustomUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Universite_Web.Models
+{
+    public class CustomUser:IdentityUser
+    {
+        [MaxLength(250)]
+        public string Surname { get; set; }
+        [MaxLength(250)]
+        public string Name { get; set; }
+      
+    }
+}
